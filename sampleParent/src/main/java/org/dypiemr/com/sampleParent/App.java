3fb2c025cd4eac1de539a09670090cc076dd2fb7
Nalini Jagtap <59967760+NaliniJagtap@users.noreply.github.com>
2020-05-10 23:08:20 +0530

    1.4 Parent-Child
    Create a project with 2 independent modules where each module will have its own pom.xml and main project pom.xml is configured as parent.

---
+++ sampleParent/src/main/java/org/dypiemr/com/sampleParent/App.java
@@ -0,0 +1,13 @@
+package org.dypiemr.com.sampleParent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
