8fb0872cc34b63cf0c005d05f793975dc8c08485
Nalini Jagtap <59967760+NaliniJagtap@users.noreply.github.com>
2020-05-10 22:58:09 +0530

    1.1 Maven Command Line
    Create a Maven archetype project from command line providing groupId and artifactId as input parameters.

---
+++ org.example.assignment1_1/src/main/java/org/example/assignment1_1/App.java
@@ -0,0 +1,13 @@
+package org.example.assignment1_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
