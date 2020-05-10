b734c254ce8b7d6c9ea0f9d6058956a953fc0393
Nalini Jagtap <59967760+NaliniJagtap@users.noreply.github.com>
2020-05-10 23:06:52 +0530

    1. 3 Jar file of 1.2 to be used
    Create a different project, add above project as dependency. Develop an application using above APIs from the above jar, and check if it's working fine at compile time and run time.

---
+++ sample-calculator-jar-assign3/src/main/java/org/dypiemr/com/sample_calculator_jar_assign3/Appln.java
@@ -0,0 +1,16 @@
+package org.dypiemr.com.sample_calculator_jar_assign3;
+
+import org.dypiemr.com.Sample_calculator_assign2.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class Appln 
+{
+    public static void main( String[] args )
+    {
+    	double result = App.add(5, 6);
+        System.out.println( "Hello ! : Calculator ADD "+ result );
+    }
+}
---
+++ sample-calculator-jar-assign3/src/test/java/org/dypiemr/com/sample_calculator_jar_assign3/ApplnTest.java
@@ -0,0 +1,20 @@
+package org.dypiemr.com.sample_calculator_jar_assign3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApplnTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
